f2c0c471414a58c2347e63455c54810755304b60
dumenko111 <101402719+dumenko111@users.noreply.github.com>
2023-03-01 20:02:53 +0000

    Deploying to gh-pages from @ dumenko111/training-timer@8d3512af3afa7ee0e9712d660028eae060b7788d 🚀

---
+++ 03-timer.091cd480.js
@@ -0,0 +1,2 @@
+class CountdownTimer{constructor(){}}new CountdownTimer({selector:"#timer-1",targetDate:new Date("Jul 17, 2019")});const days=Math.floor(time/864e5),hours=Math.floor(time%864e5/36e5),mins=Math.floor(time%36e5/6e4),secs=Math.floor(time%6e4/1e3);
+//# sourceMappingURL=03-timer.091cd480.js.map
