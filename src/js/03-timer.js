880b53529081e633c0a140ecd0a94c590f08aa32
Andriy Dumenko <101402719+dumenko111@users.noreply.github.com>
2023-03-02 10:12:25 +0200

    add timer

--- src/js/03-timer.js
+++ src/js/03-timer.js
@@ -7,46 +7,46 @@
  Плагин ожидает следующую HTML-разметку и показывает четыре цифры: 
  дни, часы, минуты и секунды в формате XX:XX:XX:XX. 
  Количество дней может состоять из более чем двух цифр.*/
-
-
-
+const refs = {
+  daysRefs: document.querySelector('span[data-value="days"]'),
+  hoursRefs: document.querySelector('span[data-value="hours"]'),
+  minsRefs: document.querySelector('span[data-value="mins"]'),
+  secsRefs: document.querySelector('span[data-value="secs"]'),
+}
 
  //Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
 class CountdownTimer {
-  constructor() {
+  constructor({selector, targetDate}) {
+    this.selector = selector
+    this.targetDate = targetDate
+  }
 
+    start() {
+    setInterval(() => {
+      const currentDate = Date.now()
+      const deltaTime = this.targetDate - currentDate;
+      this.updateCounter(deltaTime)
+    }, 1000)
+  }
+
+  updateCounter(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)); 
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    // console.log(`${days}: ${hours}: ${mins}: ${secs}`)
+
+    refs.daysRefs.textContent = `${days}`
+    refs.hoursRefs.textContent = `${hours}`
+    refs.minsRefs.textContent = `${mins}`
+    refs.secsRefs.textContent = `${secs}`
   }
 } 
 
 
-new CountdownTimer({
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Apr 01, 2023'),
  });
-
-
-
- /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+timer.start()
